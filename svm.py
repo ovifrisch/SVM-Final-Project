da6098773b26aa5b26d562de328fc9d8cb88d3ce
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-06-02 23:11:05 -0700

    first working implementation

--- svm.py
+++ svm.py
@@ -1,7 +1,6 @@
 import numpy as np
 from kernels import Kernel
-
-
+import random
 
 class svm:
 	"""
@@ -24,6 +23,8 @@ class svm:
 		"""
 		self.__kernel = Kernel(kernel_type, gamma, degree)
 		self.__C = C
+		self.__tol = 0.01
+		self.__max_passes = 500
 
 	# Accepts X and Y training data and creates the model using SMO
 	def fit(self, X, y):
@@ -45,16 +46,24 @@ class svm:
 		self.ys = y
 		self.__size = X.shape[0]
 
-		# Initialize some constants for SMO
-		self.__init_As()
-		self.__init_Bs()
-
 		# Computationally Intensive SMO
-		self.alphas = self.__sequential_minimal_optimization()
+		self.__smo()
 
-		# Extract support vectors and b
-		self.support_vectors, self.support_vector_classes = self.__get_support_vectors()
-		self.__b = self.__get_b()
+	def __train_predict(self, xs):
+		preds = np.zeros((xs.shape[0])) # predictions
+		for i in range(xs.shape[0]):
+			x = xs[i, :]
+			preds[i] = self.__sum_w_tranpose_x(x) + self.__b
+		return preds
+
+	def __objective(self):
+		a = sum(self.alphas)
+		b = 0
+		for i in range(self.__size):
+			for j in range(self.__size):
+				b += self.ys[i]*self.ys[j]*self.alphas[i]*self.alphas[j]*self.__kernel.apply(self.xs[i, :], self.xs[j, :])
+		return a - (0.5 * b)
+
 
 	# Accepts a list of testing samples and returns the predicted classes
 	# Modeled after discriminant function in (6)
@@ -74,41 +83,16 @@ class svm:
 			preds[i] = np.sign(self.__sum_w_tranpose_x(x) + self.__b)
 		return preds
 
+
 	def __sum_w_tranpose_x(self, x):
 		"""
 		Implements the summation part of (6) in BTL using only the support vectors
 		"""
-		kernel_vector = np.apply_along_axis(self.__kernel.apply, 1, self.support_vectors, x2=x)
+		kernel_vector = np.apply_along_axis(self.__kernel.apply, 1, self.xs, x2=x)
 		zipped = zip(kernel_vector, self.ys, self.alphas)
 		products = [np.prod(tup) for tup in zipped]
 		return sum(products)
 
-	def __get_b(self):
-		"""
-		Calculates B from the constraint equation using an arbitarary support vector
-		"""
-		return self.support_vector_classes[0] - self.__sum_w_tranpose_x(self.support_vectors[0, :])
-
-	# need to test this
-	def __get_support_vectors(self):
-		"""
-		Return : (ndarray, ndarray)
-			The support vectors for this classifier and their corresponding class membership (+1 or -1)
-		"""
-		num_supp_vecs = np.nonzero(self.alphas)
-		supp_vecs = np.matrix((num_supp_vecs, self.xs.shape[1]))
-		supp_vecs_idxs = np.matrix((num_supp_vecs, 1))
-		idx2 = 0
-		for i in range(self.__size):
-			if (self.alphas[i] != 0):
-				supp_vecs[idx2, :] = self.xs[i, :]
-				supp_vecs_idxs[idx2] = i
-				idx2 += 1
-		return supp_vecs, supp_vecs_idxs
-
-	def test_conditions(self):
-		s = sum(list(map(lambda a, y: a*y, self.alphas, self.ys)))
-		print(s)
 
 	def __validate_fit_params(self, X, y):
 		"""
@@ -140,107 +124,66 @@ class svm:
 		return True
 
 
-	def __init_As(self):
-		"""
-		Sets self.__As
-		self.__As is a list where __As[i] is the lower bound for alphas[i] * ys[i]
-		self.__As[i] is -C if ys[i] == -1
-		self.__As[i] is  0 if ys[i] == +1
-		"""
-		self.__As = []
-		for i in range(self.__size):
-			if (self.ys[i] == -1):
-				self.__As.append(-self.__C)
+
+	def __smo(self):
+		self.alphas = [0] * self.__size
+		self.__b = 0
+		passes = 0
+		while (passes < self.__max_passes):
+			print("Pass " + str(passes) + "/" + str(self.__max_passes))
+			num_changed_alphas = 0
+			for i in range(self.__size):
+				Ei = self.__train_predict(self.xs[i, :])[0] - self.ys[i]
+				if ((self.ys[i] * Ei < -self.__tol and self.alphas[i] < self.__C) or (self.ys[i] * Ei > self.__tol and self.alphas[i] > 0)):
+					while (True):
+						j = random.randint(0, self.__size - 1)
+						if (j != i):
+							break
+
+					Ej = self.__train_predict(self.xs[j, :])[0] - self.ys[j]
+					alpha_i_old = self.alphas[i]
+					alpha_j_old = self.alphas[j]
+
+					s = self.ys[i] * self.ys[j]
+					if (s > 0):
+						L = max(0, self.alphas[i] + self.alphas[j] - self.__C)
+						H = min(self.__C, self.alphas[i] + self.alphas[j])
+					else:
+						L = max(0, self.alphas[i] - self.alphas[j])
+						H = min(self.__C, self.__C + self.alphas[j] - self.alphas[i])
+					if (L == H):
+						continue
+
+					n = (2*self.__kernel.apply(self.xs[i, :], self.xs[j, :])) - self.__kernel.apply(self.xs[i, :], self.xs[i, :]) - self.__kernel.apply(self.xs[j, :], self.xs[j, :])
+					if (n >= 0):
+						continue
+
+					self.alphas[j] -= (self.ys[j] * (Ei - Ej)) / n
+					if (self.alphas[j] > H):
+						self.alphas[j] = H
+					elif (self.alphas[j] < L):
+						self.alphas[j] = L
+
+
+					if (abs(self.alphas[j] - alpha_j_old) < 0.00001):
+						continue
+
+					self.alphas[i] += s*(alpha_j_old - self.alphas[j])
+					b1 = self.__b - Ei - (self.ys[i] * (self.alphas[i] - alpha_i_old) * self.__kernel.apply(self.xs[i, :], self.xs[i, :])) - (self.ys[j] * (self.alphas[j] - alpha_j_old) * self.__kernel.apply(self.xs[i, :], self.xs[j, :]))
+					b2 = self.__b - Ej - (self.ys[i] * (self.alphas[i] - alpha_i_old) * self.__kernel.apply(self.xs[i, :], self.xs[j, :])) - (self.ys[j] * (self.alphas[j] - alpha_j_old) * self.__kernel.apply(self.xs[j, :], self.xs[j, :]))
+
+					if (self.alphas[i] > 0 and self.alphas[i] < self.__C):
+						self.__b = b1
+					elif (self.alphas[j] > 0 and self.alphas[j] < self.__C):
+						self.__b = b2
+					else:
+						self.__b = (b1 + b2) / 2
+
+					num_changed_alphas  += 1
+				# end if
+			# end for
+
+			if (num_changed_alphas == 0):
+				passes += 1
 			else:
-				self.__As.append(0)
-
-	def __init_Bs(self):
-		"""
-		Sets self.__Bs
-		self.__Bs is a list where __As[i] is the upper bound for alphas[i] * ys[i]
-		self.__Bs[i] is 0 if ys[i] == -1
-		self.__Bs[i] is C if ys[i] == +1
-		"""
-		self.__Bs = []
-		for i in range(self.__size):
-			if (self.ys[i] == 1):
-				self.__Bs.append(self.__C)
-			else:
-				self.__Bs.append(0)
-
-
-	def __update_gradient(self, i, j, k, gradients, lamb):
-		"""
-		Implements Line 8 of SMO
-		"""
-		return gradients[k] - (lamb * self.ys[k] * self.__kernel.apply(self.xs[i, :], self.xs[k, :])) + (lamb * self.ys[k] * self.__kernel.apply(self.xs[j, :], self.xs[k, :]))
-
-	def __get_lambda(self, i, j, gradients, alphas):
-		"""
-		Implements Line 7 of SMO
-		"""
-		arg1 = self.__Bs[i] - (self.ys[i] * alphas[i])
-		arg2 = (self.ys[j] * alphas[j]) - self.__As[j]
-		yigi = self.ys[i] * gradients[i]
-		yjgj = self.ys[j] * gradients[j]
-		Kii = self.__kernel.apply(self.xs[i, :], self.xs[i, :])
-		Kjj = self.__kernel.apply(self.xs[j, :], self.xs[j, :])
-		Kij = self.__kernel.apply(self.xs[i, :], self.xs[j, :])
-		arg3 = (yigi - yjgj) / (Kii + Kjj - (2*Kij))
-		return min(arg1, min(arg2, arg3))
-
-	def __get_i(self, gradients, alphas):
-		"""
-		Implements Line 4 of SMO
-		"""
-		temp = list(map(lambda y, g: y*g, self.ys, gradients))
-		max_idx = 0
-		max_el = temp[0]
-		for i in range(len(temp)):
-			if (temp[i] > max_el and self.ys[i]*alphas[i] < self.__Bs[i]):
-				max_el = temp[i]
-				max_idx = i
-		return max_idx
-
-
-	def __get_j(self, gradients, alphas):
-		"""
-		Implements Line 5 of SMO
-		"""
-		temp = list(map(lambda y, g: y*g, self.ys, gradients))
-		min_idx = 0
-		min_el = temp[0]
-		for j in range(len(temp)):
-			if (temp[j] < min_el and self.ys[j]*alphas[j] > self.__As[j]):
-				min_el = temp[j]
-				min_idx = j
-		return min_idx
-
-	def __optimality_criterion(self, i, j, gradients):
-		"""
-		Implements Line 6 of SMO
-		"""
-
-		# This is the optimality criterion (11)
-		print(self.ys[i] * gradients[i])
-		print("must be less than")
-		print(self.ys[j] * gradients[j])
-		return (self.ys[i] * gradients[i] <= self.ys[j] * gradients[j])
-
-	def __sequential_minimal_optimization(self):
-		"""
-		This function implements the SMO algorithm from Bottou Lin 2006 (BTL) section 6.3
-		It maximizes the objective function and finds the Lagrange Multipliers (denoted alphas here)
-		"""
-		alphas = [0] * self.__size # Line 1
-		gradients = [1] * self.__size # Line 2
-		while (True):
-			i = self.__get_i(gradients, alphas)
-			j = self.__get_j(gradients, alphas)
-			if (self.__optimality_criterion(i, j, gradients)):
-				return alphas
-			lambda_ = self.__get_lambda(i, j, gradients, alphas)
-			for k in range(self.__size):
-				gradients[k] = self.__update_gradient(i, j, k, gradients, lambda_)
-				alphas[i] = alphas[i] + (self.ys[i] * lambda_) # Line 9
-				alphas[j] = alphas[j] - (self.ys[j] * lambda_) # Line 9
+				passes += 1
--- test_svm.py
+++ test_svm.py
@@ -2,16 +2,29 @@ import svm
 import numpy as np
 from sklearn.svm import SVC
 from kernels import Kernel
+from sklearn.utils import shuffle
 
-num_samples = 1000
+num_samples = 10
 
 if __name__ == "__main__":
 	clf = svm.svm("rbf", 1, 1)
 	posX = np.random.normal(loc=0, scale = 1, size=int(num_samples/2))
-	negX = np.random.normal(loc=100, scale=1, size=int(num_samples/2))
+	negX = np.random.normal(loc=10, scale=1, size=int(num_samples/2))
 	X = np.transpose(np.matrix(np.append(posX, negX)))
 	ys = np.append(np.ones(int(num_samples / 2)), (-1 * np.ones(int(num_samples / 2))))
-	# clf.fit(X, ys)
+	clf.fit(X, ys)
+
+	posX_test = np.random.normal(loc=0, scale = 1, size=int(num_samples / 2))
+	negX_test = np.random.normal(loc=10, scale = 1, size=int(num_samples / 2))
+	X_test = np.append(posX_test, negX_test)
+	X_test, ys = shuffle(X_test, ys)
+	print(X_test.shape)
+	print(ys.shape)
+	pred_ys = clf.predict(np.transpose(np.matrix(X_test)))
+
+	acc = np.sum(ys==pred_ys) / num_samples
+	print("Accuracy: " + str(acc))
+
 	# alphas = clf.alphas
 	# pos = alphas[0:int(len(alphas) / 2)]
 	# neg = alphas[int(len(alphas) / 2):]
@@ -20,14 +33,6 @@ if __name__ == "__main__":
 	# print(pos_supp, neg_supp)
 	#
 	#
-	clf2 = SVC(kernel="rbf")
-	clf2.fit(X, ys)
+	# clf2 = SVC(kernel="rbf")
+	# clf2.fit(X, ys)
 	# print(clf2.n_support_)
-
-	# posX_test = np.random.normal(loc=0, scale = 1, size=int(num_samples / 2))
-	# negX_test = np.random.normal(loc=4, scale = 1, size=int(num_samples / 2))
-	# X_test = np.append(posX_test, negX_test)
-	# pred_ys = clf.predict(np.transpose(np.matrix(X_test)))
-	#
-	# acc = np.sum(ys==pred_ys) / num_samples
-	# print("Accuracy: " + str(acc))
