23af53fff5e811d12248d0149cac5fbffab2de4d
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-06-04 15:01:34 -0700

    implemented platt with error heuristic

--- svm.py
+++ svm.py
@@ -5,205 +5,64 @@ import random
 
 
 class svm:
-	"""
-	A class used to perform binary (and multiclass) classification using
-	a Support Vector Machine (SVM)
-	"""
 
-	def __init__(self, kernel_type='rbf', C=1, gamma=1, degree=3):
-		"""
-		Parameters
-		----------
-		kernel_type : str
-			The type of kernel to be used (support for "rbf", "linear", "poly")
-		C : float
-			Parameter for controlling compromise between soft and hard margin violations
-		gamma : float
-			Kernel coefficient for RBF kernel
-		degree: float
-			Exponent for ploynomial kernel
-		"""
+	def __init__(self, kernel_type='rbf', C=1, gamma=1, degree=3, tolerance=0.1, epsilon=0.1):
 		self.__kernel = Kernel(kernel_type, gamma, degree)
 		self.__C = C
-		self.__tol = 1
+		self.__tol = 0.001
+		self.__error_cache = {}
+		self.__eps = 0.001
 
 	# Accepts X and Y training data and creates the model using SMO
 	def fit(self, X, y):
-		"""
-		Parameters
-		----------
-		X : ndarray
-			Training x
-		y : ndarray
-			Training y
-		"""
-
-		# first make sure params are good
-		if (not self.__validate_fit_params(X, y)):
-			print("Bad params")
-			return
 
 		self.xs = X
 		self.ys = y
 		self.__size = X.shape[0]
 
-		# Initialize some constants for SMO
-		self.__init_As()
-		self.__init_Bs()
-
 		# Computationally Intensive SMO
 		self.alphas = [0] * self.__size
+		self.__b = 0
 		self.__sequential_minimal_optimization()
-
-		# Extract support vectors and b
-		self.support_vectors, self.support_vector_classes = self.__get_support_vectors()
-		self.__b = self.__get_b()
 
 	# Accepts a list of testing samples and returns the predicted classes
 	# Modeled after discriminant function in (6)
 	def predict(self, xs):
-		"""
-		Parameters
-		----------
-		xs : ndarray
-			testing samples
-
-		Return : ndarray
-			predicted classes of testing samples
-		"""
 		preds = np.zeros((xs.shape[0])) # predictions
 		for i in range(xs.shape[0]):
-			x = xs[i, :]
-			preds[i] = np.sign(self.__sum_w_tranpose_x(x) + self.__b)
+			x = xs[i]
+			preds[i] = np.sign(self.__sum_w_tranpose_x(x) - self.__b)
 		return preds
 
 	def __sum_w_tranpose_x(self, x):
-		"""
-		Implements the summation part of (6) in BTL using only the support vectors
-		"""
-		kernel_vector = np.apply_along_axis(self.__kernel.apply, 1, self.support_vectors, x2=x)
+		kernel_vector = np.apply_along_axis(self.__kernel.apply, 1, self.xs, x2=x)
 		zipped = zip(kernel_vector, self.ys, self.alphas)
 		products = [np.prod(tup) for tup in zipped]
 		return sum(products)
 
-	def __get_b(self):
-		"""
-		Calculates B from the constraint equation using an arbitarary support vector
-		"""
-
-		## https://github.com/LasseRegin/SVM-w-SMO/blob/master/SVM.py
-		## ^^ here it says to take mean of below expression for all support vectors,
-
-		## but in Platt 1998 it says any support vector will do
-
-		# currently sticking with Platt
-
-		# for training
-		if (self.support_vectors.shape[0] == 0):
-			return 0
-		return self.support_vector_classes[0] - self.__sum_w_tranpose_x(self.support_vectors[0, :])
-
-	# need to test this
-	def __get_support_vectors(self):
-		"""
-		Return : (ndarray, ndarray)
-			The support vectors for this classifier and their corresponding class membership (+1 or -1)
-		"""
-		num_supp_vecs = np.nonzero(self.alphas)
-		supp_vecs = np.matrix((num_supp_vecs, self.xs.shape[1]))
-		supp_vecs_idxs = np.matrix((num_supp_vecs, 1))
-		idx2 = 0
+	def __initialize_error_cache(self):
 		for i in range(self.__size):
-			if (self.alphas[i] != 0):
-				supp_vecs[idx2, :] = self.xs[i, :]
-				supp_vecs_idxs[idx2] = i
-				idx2 += 1
-		return supp_vecs, supp_vecs_idxs
-
-	def test_conditions(self):
-		s = sum(list(map(lambda a, y: a*y, self.alphas, self.ys)))
-		print(s)
-
-	def __validate_fit_params(self, X, y):
-		"""
-		Parameters
-		----------
-		X : ndarray
-			Training X
-		y : ndarray
-			Training y
-
-		Return
-		------
-		True if parameters are valid
-		False otherwise
-		"""
-		# need mxn matrix
-		if (len(X.shape) != 2):
-			return False
-
-		# y must have 1 column
-		if (len(y.shape) > 1):
-			return False
-
-		# number of rows in x must equal number of rows in y
-		if (X.shape[0] != y.shape[0]):
-			return False
-
-		# Everything good
-		return True
-
-
-	def __init_As(self):
-		"""
-		Sets self.__As
-		self.__As is a list where __As[i] is the lower bound for alphas[i] * ys[i]
-		self.__As[i] is -C if ys[i] == -1
-		self.__As[i] is  0 if ys[i] == +1
-		"""
-		self.__As = []
-		for i in range(self.__size):
-			if (self.ys[i] == -1):
-				self.__As.append(-self.__C)
-			else:
-				self.__As.append(0)
-
-	def __init_Bs(self):
-		"""
-		Sets self.__Bs
-		self.__Bs is a list where __As[i] is the upper bound for alphas[i] * ys[i]
-		self.__Bs[i] is 0 if ys[i] == -1
-		self.__Bs[i] is C if ys[i] == +1
-		"""
-		self.__Bs = []
-		for i in range(self.__size):
-			if (self.ys[i] == 1):
-				self.__Bs.append(self.__C)
-			else:
-				self.__Bs.append(0)
-
-	def list_nonzero_non_c(self):
-		res = []
-		for i in range(self.__size):
-			if (self.alphas[i] != 0 and self.alphas[i] != self.__C):
-				res.append(alphas[i])
-
-		return res
+			self.__error_cache[i] = -self.ys[i]
 
 	def takeStep(self, i1, i2, E2):
-		if (i1 == i2):
+		if (i1 == i2 or i1 == -1):
 			return 0
 		alph1 = self.alphas[i1]
+		alph2 = self.alphas[i2]
 		y1 = self.ys[i1]
 		y2 = self.ys[i2]
-		E1 = self.predict(self.xs[i1, :]) - y1
+		E1 = self.__error_cache[i1]
 		s = y1*y2
+
+		# y1 == y2
 		if (s > 0):
+			L = max(0, self.alphas[i2] - self.alphas[i1] - self.__C)
+			H = min(self.__C, self.alphas[i2] + self.alphas[i1])
+
+		# y1 != y2
+		else:
 			L = max(0, self.alphas[i2] - self.alphas[i1])
 			H = min(self.__C, self.__C + self.alphas[i2] - self.alphas[i1])
-		else:
-			L = max(0, self.alphas[i2] + self.alphas[i1] - self.__C)
-			H = min(self.__C, self.alphas[i2] + self.alphas[i1])
 		if (L == H):
 			return 0
 		k11 = self.__kernel.apply(self.xs[i1, :], self.xs[i1, :])
@@ -217,8 +76,13 @@ class svm:
 			elif (a2 > H):
 				a2 = H
 		else:
-			Lobj = 1# objective function at a2=L
-			Hobj = 1# objective function at a2=H
+			f1 = (y1*(E1 + self.__b)) - (self.alphas[i1]*k11) - (s*self.alphas[i2]*k12)
+			f2 = (y2*(E2 + self.__b)) - (s*self.alphas[i1]*k12) - (self.alphas[i2]*k22)
+			L1 = self.alphas[i1] + (s*(self.alphas[i2] - L))
+			H1 = self.alphas[i1] + (s*(self.alphas[i2] - H))
+			Lobj = L1*f1 + L*f2 + 0.5*pow(L1, 2)*k11 + 0.5*pow(L, 2)*k22 + s*L*L1*k12
+			Hobj = H1*f1 + H*f2 + 0.5*pow(H1, 2)*k11 + 0.5*pow(H, 2)*k22 + s*H*H1*k12
+
 			if (Lobj < Hobj - self.__eps):
 				a2 = L
 			elif (Lobj < Hobj+self.__eps):
@@ -229,55 +93,87 @@ class svm:
 		if (abs(a2 - alph2) < self.__eps * (a2 + alph2 + self.__eps)):
 			return 0
 		a1 = alph1 + s*(alph2 - a2)
+		if (a1 < 0):
+			a2 += s * a1
+			a1 = 0
+		elif (a1 > self.__C):
+			a2 += s * (a1 - self.__C)
+			a1 = self.__C
+
 		# Update threshold to reflect change in Lagrange multipliers
+		b1 = self.__b + E1 + y1*(a1 - alph1)*k11 + y2*(a2 - alph2)*k12
+		b2 = self.__b + E2 + y1*(a1 - alph1)*k12 + y2*(a2 - alph2)*k22
+		b_new = (b1 + b2) / 2
+		delta_b = b_new = self.__b
+		self.__b = b_new
+
+
 		# Update weight vector to reflect change in a1 & a2, if SVM is linear
-		# Update weight vector to reflect change in a1 & a2, if SVM is linear
+
+		# Update error cache using new Lagrange multipliers
+		t1 = y1 * (a1 - alph1)
+		t2 = y2 * (a2 - alph2)
+
+		for i in range(self.__size):
+			if (self.alphas[i] > 0 and self.alphas[i] < self.__C):
+				self.__error_cache[i] += t1 * self.__kernel.apply(self.xs[i1, :], self.xs[i, :]) + t2 * self.__kernel.apply(self.xs[i2, :], self.xs[i, :]) - delta_b
+
+		self.__error_cache[i1] = 0
+		self.__error_cache[i2] = 0
+
+		# store new alphas
 		self.alphas[i1] = a1
 		self.alphas[i2] = a2
 		return 1
 
+	def __second_choice_heuristic(self, E2):
+		tmax = 0
+		i1 = -1
+		for k in range(self.__size):
+			if (self.alphas[k] > 0 and self.alphas[k] < self.__C):
+				E1 = self.__error_cache[k]
+				temp = abs(E2 - E1)
+				if (temp > tmax):
+					tmax = temp
+					i1 = k
+		return i1
 
 
 	def examineExample(self, i2):
 		y2 = self.ys[i2]
 		alph2 = self.alphas[i2]
-		E2 = self.predict(self.xs[i2, :]) - y2
+		E2 = self.predict(self.xs[i2, :])[0] - y2
 		r2 = E2*y2
-		if ((r < -self.__tol and alph2 < self.__C) or (r2 > self.__tol and alph2 > 0)):
-			if (len(self.list_nonzero_non_c()) > 1):
-				i1 = 1# reult of second choice heuristic
-				if (self.takeStep(i1, i2)):
-					return 1
+		if ((r2 < -self.__tol and alph2 < self.__C) or (r2 > self.__tol and alph2 > 0)):
+			i1 = self.__second_choice_heuristic(E2)
+			if (self.takeStep(i1, i2, E2)):
+				return 1
 			startIdx1 = random.randint(0, self.__size)
-			for i1 in range(startIdx, self.__size):
-				if (self.alphas[i1] == 0 or self.alphas[i1] == self.__C):
-					continue
-				if (takeStep(i1, i2, E2)):
-					return 1
-			for i1 in range(0, startIdx):
-				if (self.alphas[i1] == 0 or self.alphas[i1] == self.__C):
-					continue
-				if (takeStep(i1, i2, E2)):
-					return 1
+			for i1 in range(startIdx1, self.__size):
+				if (self.alphas[i1] > 0 and self.alphas[i1] < self.__C):
+					if (self.takeStep(i1, i2, E2)):
+						return 1
+			for i1 in range(0, startIdx1):
+				if (self.alphas[i1] > 0 and self.alphas[i1] < self.__C):
+					if (self.takeStep(i1, i2, E2)):
+						return 1
 
 			startIdx2 = random.randint(0, self.__size)
 			for i1 in range(startIdx2, self.__size):
-				if (takeStep(i1, i2)):
+				if (self.takeStep(i1, i2, E2)):
 					return 1
 			for i1 in range(0, startIdx2):
-				if (takeStep(i1, i2)):
+				if (self.takeStep(i1, i2, E2)):
 					return 1
 		return 0
-
-
-
-
 
 
 	def __sequential_minimal_optimization(self):
 		numChanged = 0
 		examineAll = 1
+		self.__initialize_error_cache()
 		while (numChanged > 0 or examineAll):
+			numChanged = 0
 			if (examineAll):
 				for i in range(self.__size):
 					numChanged += self.examineExample(i)
--- test_svm.py
+++ test_svm.py
@@ -1,43 +1,71 @@
 import svm
 import numpy as np
 from sklearn.svm import SVC
-from kernels import Kernel
+import time
 
-num_samples = 1000
+class SVM_Tester:
+	def __init__(self, num_dimensions = 1, num_samples = 100, loc1 = 1, loc2 = 4,
+	tolerance=0.1, epsilon=0.1, C=0.05, kernel = "rbf", gamma = 1, degree = 3):
+
+		self.loc1 = loc1
+		self.loc2 = loc2
+		self.num_samples = num_samples
+		self.num_dimensions = num_dimensions
+		self.kernel = kernel
+		self.C = C
+		self.gamma = gamma
+		self.degree = degree
+		self.tolerance = tolerance
+		self.epsilon = epsilon
+		self.train_xs = self.generate_xs()
+		self.test_xs = self.generate_xs()
+		self.ys = np.append(np.ones(int(num_samples / 2)), (-1 * np.ones(int(num_samples / 2))))
+		self.clf = svm.svm(self.kernel, self.C, self.gamma, self.degree, self.tolerance, self.epsilon)
+	def __del__(self):
+		pass
+
+	def generate_xs(self):
+		xs = np.zeros((self.num_samples, self.num_dimensions))
+		for i in range(self.num_dimensions):
+			posX = np.random.normal(loc=self.loc1, scale = 1, size=int(self.num_samples/2))
+			negX = np.random.normal(loc=self.loc2, scale = 1, size=int(self.num_samples/2))
+			X_i = np.transpose(np.matrix(np.append(posX, negX)))
+			xs[:, i:i+1] = X_i
+		return xs
+
+	def train(self):
+		start_time = time.time()
+		self.clf.fit(self.train_xs, self.ys)
+		self.train_time = time.time() - start_time
+
+	def test(self):
+		start_time = time.time()
+		pred_ys = self.clf.predict(self.test_xs)
+		self.test_time = time.time() - start_time
+		acc = np.sum(self.ys==pred_ys) / self.num_samples
+		print_results("Our Accuracy: ", acc, self.train_time, self.test_time)
+	def get_training_data(self):
+		return self.train_xs, self.ys
+
+	def get_testing_data(self):
+		return self.test_xs, self.ys
+
+def print_results(whos, acc, train_time, test_time):
+	print(whos + str(acc) + "  Train time: " + str(round(train_time, 3)) + "s  Test time: " + str(round(test_time, 3)) + "s")
 
 if __name__ == "__main__":
-	clf = svm.svm("rbf", 1, 1)
-	posX = np.random.normal(loc=0, scale = 1, size=int(num_samples/2))
-	negX = np.random.normal(loc=5, scale=1, size=int(num_samples/2))
-	X = np.transpose(np.matrix(np.append(posX, negX)))
-	ys = np.append(np.ones(int(num_samples / 2)), (-1 * np.ones(int(num_samples / 2))))
-	clf.fit(X, ys)
+	# Our SVM
+	tester = SVM_Tester(num_dimensions=1, num_samples=100)
+	tester.train()
+	tester.test()
 
-
-
-
-
-
-
-
-
-	# initialize b to 0
-	# alphas = clf.alphas
-	# pos = alphas[0:int(len(alphas) / 2)]
-	# neg = alphas[int(len(alphas) / 2):]
-	# pos_supp = len([x for x in pos if x != 0])
-	# neg_supp = len([x for x in neg if x != 0])
-	# print(pos_supp, neg_supp)
-	# #
-	# #
-	# # clf2 = SVC(kernel="rbf")
-	# # clf2.fit(X, ys)
-	# # print(clf2.n_support_)
-
-	posX_test = np.random.normal(loc=0, scale = 1, size=int(num_samples / 2))
-	negX_test = np.random.normal(loc=4, scale = 1, size=int(num_samples / 2))
-	X_test = np.append(posX_test, negX_test)
-	pred_ys = clf.predict(np.transpose(np.matrix(X_test)))
-
-	acc = np.sum(ys==pred_ys) / num_samples
-	print("Accuracy: " + str(acc))
+	# Sci Kit Learn SVM
+	clf2 = SVC(kernel="rbf", C=tester.C, gamma=tester.gamma, tol=tester.tolerance)
+	start_time = time.time()
+	clf2.fit(tester.get_training_data()[0], tester.get_training_data()[1])
+	train_time = time.time() - start_time
+	start_time = time.time()
+	pred_ys = clf2.predict(tester.get_testing_data()[0])
+	test_time = time.time() - start_time
+	acc = np.sum(tester.get_testing_data()[1]==pred_ys) / tester.num_samples
+	print_results("Scikit Accuracy: ", acc, train_time, test_time)
